60c178589b15e587b0e106cabe1b39bf842df85a
Debjit Mondal <debjit6296@gmail.com>
2026-09-24 00:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (71.69%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,38 @@
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 2;
+    result[0] = -1;
+    result[1] = -1;
+
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            result[0] = mid;
+            right = mid - 1;
+        }
+    }
+
+    left = 0;
+    right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            result[1] = mid;
+            left = mid + 1;
+        }
+    }
+
+    return result;
+}
